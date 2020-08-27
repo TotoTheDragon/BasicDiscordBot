1c8f76236fd4d17ab000acc47736967bbd0fbc65
Dragon <MrDragonPlays@users.noreply.github.com>
2020-08-27 22:53:55 +0200

    Added commandinfo to the wrappedmessage

--- src/objects/WrappedMessage.ts
+++ src/objects/WrappedMessage.ts
@@ -1,8 +1,10 @@
 import { Message } from "discord.js";
 import { Settings } from "./Settings";
+wrawimport { CommandInfo } from "./commands/CommandInfo";
 
 export class WrappedMessage extends Message {
 
     settings: Settings;
+    info: CommandInfo;
 
 }
