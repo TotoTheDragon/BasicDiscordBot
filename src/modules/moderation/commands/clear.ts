e93a7e77fe22b9e75510363c0966680b0297b45f
Dragon <MrDragonPlays@users.noreply.github.com>
2020-08-27 22:55:21 +0200

    Added logging to command

--- src/modules/moderation/commands/clear.ts
+++ src/modules/moderation/commands/clear.ts
@@ -2,6 +2,8 @@ import { WrappedClient } from "../../../client";
 import { IntegerArgument } from "../../../objects/commands/arguments/IntegerArgument";
 import { Command } from "../../../objects/commands/Command";
 import { CommandInfo } from "../../../objects/commands/CommandInfo";
+import { TextChannel, MessageEmbed } from "discord.js";
+import { footer, footerurl } from "../../../config";
 
 export class Clear implements Command {
     label = "clear";
@@ -19,8 +21,30 @@ export class Clear implements Command {
 
         if (!info.message.deleted) await info.message.delete();
 
-        await info.guildchannel.bulkDelete(mappedArgs.get("amount") as number);
+        const deleted = await info.guildchannel.bulkDelete(mappedArgs.get("amount") as number);
 
+        const logChannel = info.findChannelById(info.message.settings.get("moderation", "log-channel"));
+
+        if (logChannel !== undefined)
+            await logChannel.send(
+                new MessageEmbed()
+                    .setTitle("Cleared messages")
+                    .setDescription(`A total of ${deleted.size} messages were deleted from channel <#${info.channel.id}>`)
+                    .setColor("#ff0000")
+                    .addField("Executor", `<@${info.user.id}>`, true)
+                    .setTimestamp()
+                    .setFooter(footer, footerurl)
+            );
+        const msg = await info.guildchannel.send(
+            new MessageEmbed()
+                .setTitle("Cleared messages")
+                .setDescription(`A total of ${deleted.size} messages were deleted from channel <#${info.channel.id}>`)
+                .setColor("#ff0000")
+                .addField("Executor", `<@${info.user.id}>`, true)
+                .setTimestamp()
+                .setFooter(footer, footerurl)
+        );
+        msg.delete({ timeout: 5000 })
     }
 
 }
