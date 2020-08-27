fcf7d9a8d56e2f5e8236eccc1cac674a51bd8ddf
Dragon <MrDragonPlays@users.noreply.github.com>
2020-08-27 23:25:28 +0200

    Disabled WIP feature to allow for compilling

--- src/util/OtherUtil.ts
+++ src/util/OtherUtil.ts
@@ -1,12 +1,15 @@
-import { WrappedMessage } from "../objects/WrappedMessage";
 import { CommandInfo } from "../objects/commands/CommandInfo";
 
 export function getUserLevel(info: CommandInfo): number {
 
+    /*
     let levels = info.member.roles
         .cache
         .map(role => info.message.settings.roleLevels.get(role.id))
         .filter(value => value != undefined);
 
+
     return levels.length ? Math.max(...levels) : 0;
+    */
+    return 0;
 }
