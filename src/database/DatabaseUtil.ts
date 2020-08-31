460302fb77196745e28274a76d09c0a100037981
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2020-08-31 12:28:42 +0200

    Started a seperate project for databases

--- src/database/DatabaseUtil.ts
+++
@@ -1,9 +0,0 @@
-import { Settings } from "../objects/bot/Settings";
-
-export async function getSettings(id: string): Promise<Settings> {
-    return undefined;
-}
-
-/*
-    All database getters/setters for other things
-*/
