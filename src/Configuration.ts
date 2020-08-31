7bcff3b6f74de295239c75a44970c7ced4b7ccd7
Dragon <42499964+TotoTheDragon@users.noreply.github.com>
2020-08-31 12:32:11 +0200

    Added admin-role to configuration while permission system is unfinished

--- src/Configuration.ts
+++ src/Configuration.ts
@@ -1,14 +1,17 @@
+import { RoleArgument } from "./objects/commands/arguments/RoleArgument";
 import { IConfiguration } from "./objects/modules/IConfiguration";
 
 export class Configuration implements IConfiguration {
 
     globalParse: Map<string, (input: string) => any> = new Map();
 
-    guildParse: Map<string, (input: string) => any> = new Map();
+    guildParse: Map<string, (input: string) => any> = new Map()
+        .set("admin-role", new RoleArgument().parseId);
 
     globalSettings = new Map()
         .set("token", "PUT YOUR TOKEN HERE");
 
     guildSettings = new Map()
-        .set("prefix", "!");
+        .set("prefix", "!")
+        .set("admin-role", "");
 }
